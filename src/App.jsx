c8d6f0e1bf517c029ac8dd16665246b7f92b1608
Josh <jarhoads@gmail.com>
2024-07-09 16:18:04 -0400

    initial commit with sidebar styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+
+function App() {
+  return (
+    <main>
+      <ProjectsSidebar className='h-screen my-8'/>
+    </main>
+  );
+}
+
+export default App;
