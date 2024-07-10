168f2abd99df8255bd1cc238c66149deffa82c37
Josh <jarhoads@gmail.com>
2024-07-10 15:59:11 -0400

    switch between form and no content page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({selectedProjectId: undefined, projects: []});
+
+  function handleStartAddProject(params) {
+    setProjectsState(prevState => { return {...prevState, selectedProjectId: null}; });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined ) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
